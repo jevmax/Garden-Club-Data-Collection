59f2be4f374f06e9bd715e3aada3fa6beb700ac7
j.Maxwell <98854348+jevmax@users.noreply.github.com>
2024-02-08 21:21:27 -0500

    Update total_Amt_v1.sql

--- Data/SQL_DB_and_QUERIES/total_Amt_v1.sql
+++ Data/SQL_DB_and_QUERIES/total_Amt_v1.sql
@@ -1,7 +1,8 @@
-Select *
+/*Getting the total amount of money */
+SELECT *
 From summer_Series.dbo.fiveTen
 Where Credits_Used > 0 ;
-/*Getting the total amount of money */
+
 
 SELECT SUM(Amount) as [Total Amount for all events:]
 FROM (
@@ -13,13 +14,15 @@ SELECT Amount  FROM summer_Series.dbo.fortyfiveFifty
 ) AS vik
 ;
 
-
+-- get the amount of totals between $5 and 10
 SELECT sum(Amount) as [ $5/$10 Events Total ]
 FROM summer_Series.dbo.fiveTen;
 
+-- $30 and $40
 SELECT sum(Amount) as [$30/$40 Events Total ]
 FROM summer_Series.dbo.thirtyForty
 
+-- $45 and $50
 SELECT sum(Amount) as [ $45/$50 Events Total ]
 FROM summer_Series.dbo.fortyfiveFifty;
 
